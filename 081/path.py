fd558c36ab8b7da01100ddc8f87eed01f48a1062
Peter J. Bates <peterbates8@gmail.com>
2017-07-09 11:53:06 -0500

    Solves minimum path and generalizes for different array sizes.

--- 081/path.py
+++ 081/path.py
@@ -1,8 +1,25 @@
 import numpy as np
+import time as t
+start = t.time()
 
-file = open("matrix.txt")
-string = file.read().replace('\n', ',').split(',')
-del string[-1]
-array = np.array(string).reshape(80,80)
+def min_path_sum(array):
+    (rows, columns) = array.shape
 
-print(array)
+    for i in range(1,columns):
+        array[0, i] += array[0, i-1]
+    for i in range(1,rows):
+        array[i, 0] += array[i-1, 0]
+
+    for i in range(1, rows):
+        for j in range(1, columns):
+            array[i,j] += min(array[i - 1, j], array[i, j - 1])
+    return array[row-1,column-1]
+
+def main(file):
+    array = np.genfromtxt(file, delimiter=",")
+
+    print(min_path_sum(array))
+    print("---%s seconds---" %(t.time()-start))
+
+if __name__ == "__main__":
+    main("matrix.txt")
