562ce0571a1acebc6b71757b45f5890bde64167b
Peter J. Bates <peterbates8@gmail.com>
2018-02-21 22:21:38 -0600

    Fixed spacing.

--- 006/sumSquare.java
+++ 006/sumSquare.java
@@ -2,15 +2,16 @@ class sumSquare {
 
 	public static int findDifference() {
 		int squareSum = 0;
-    		int sumSquare = 0;
+    int sumSquare = 0;
 
-    		for (int i = 1; i < 101; i++) {
-      			squareSum += i;
-      			sumSquare += i*i;
+		for (int i = 1; i < 101; i++) {
+  			squareSum += i;
+  			sumSquare += i*i;
     	}
-    		squareSum *= squareSum;
-    		int difference = squareSum - sumSquare;
-    		return difference;
+			
+		squareSum *= squareSum;
+		int difference = squareSum - sumSquare;
+		return difference;
 	}
 
 	public static void main(String[] args)
